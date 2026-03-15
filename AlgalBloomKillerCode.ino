7c2463421aa1ca210233d034cc9c168d6aea9ca6
bbelg-hehe <belgaiedbaya@gmail.com>
2026-03-15 17:54:36 -0400

    Add files via upload

---
+++ AlgalBloomKillerCode.ino
@@ -0,0 +1,33 @@
+#include <Servo.h>
+const int triggerPin1 = 3;
+const int triggerPin2 = 5;
+const int servo = 9;
+int delayTime = 15;
+
+Servo myServo; //Create a servo object
+int pos = 0; //Variable to store the servo position
+
+
+
+void setup() { // put your setup code here, to run once:
+ myServo.attach(9); //Attach servo to pin 9
+ pinMode(triggerPin1, OUTPUT);
+ pinMode(triggerPin2, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ digitalWrite(triggerPin1, HIGH);
+ digitalWrite(triggerPin2, HIGH);
+
+for (pos = 0;pos <= 180; pos += 1){ //Move to 180 degrees
+   myServo.write(pos);
+     delay(delayTime); //Wait for servo to reach position
+ }
+ //Move arm left
+ for(pos = 180; pos >= 0; pos -= 1){ //Move back to 0 degrees
+   myServo.write(pos);
+     delay(delayTime); //Wait for servo to reach position
+ }
+
+}
